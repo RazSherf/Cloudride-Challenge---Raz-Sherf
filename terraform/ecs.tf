57ad7a3d009867f83dd3b11aa3e12d3c0938622c
RazSherf <101274250+RazSherf@users.noreply.github.com>
2025-05-26 20:42:52 +0300

    fixed the ecs.tf file

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -17,6 +17,7 @@ resource "aws_ecs_cluster" "main" {
 resource "aws_ecs_task_definition" "hello_world" {
   family                   = "hello-world-task"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 256
